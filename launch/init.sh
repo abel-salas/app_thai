7a588088157d0a4616fa6f1ac9fd6ed273a89030
abel salas <asalas@cdmon.com>
2016-09-05 10:01:32 +0200

    add init panel

--- launch/init.sh
+++ launch/init.sh
@@ -1,26 +1,81 @@
 #!/bin/bash
+################### Functions ####################
+function copyDatabase(){
+   tar -zcvf /home/abel/Dropbox/EsteticaThai/db_$DATE.tar.gz /var/mongo/data
+   echo "Hemos finalizado la copia de seguridad en Dropbox con fecha $DATE"
+}
 
+function appStart() {
+   cd $root_directory; 
+   sudo docker-compose up -d; 
+   # chromium-browser --kiosk http://localhost:8080
+}
+
+function appStop() {
+   cd $root_directory; 
+   sudo docker-compose stop;
+}
+
+function appLogs() {
+   cd $root_directory; 
+   sudo docker-compose logs;
+}
+
+function appRestart() {
+   cd $root_directory; 
+   sudo docker-compose restart;
+}
+##################################################
+
+
+################### Variables ####################
+
+dialog_title="CDmon's Panell de Gestió Interna"
+root_directory="/home/projects/pgi_dev-environment/"
 DATE=`date +%Y-%m-%d`
 
-echo "Buenos dias Thai, hoy va ha ser un gran dia!"
+# Menu exit & cancel behavior
+dialogcancel=1
+dialogesc=255
 
-echo ""
-
-echo "La fecha de hoy es $DATE."
-
-echo ""
+# Trap and delete temp files
+trap "exit" SIGHUP SIGINT SIGTERM
+##################################################
 
 
-sudo docker-compose up -d
-
-tar -zcvf /home/abel/Dropbox/EsteticaThai/db_$DATE.tar.gz /var/mongo/data
-
-echo ""
-
-echo "Hemos finalizado la copia de seguridad en Dropbox con fecha $DATE"
-
-echo ""
-
-echo "Recuerda que para poder cerrar el terminal deberas presionar Ctrl + C"
-
-chromium-browser --kiosk http://localhost:8080
+##################### Main #######################
+while true ; do
+   exec 3>&1
+   menuitem=$(dialog --clear --backtitle "${dialog_title}" \
+   --title "[ MENU PGI ] [ Dia: ${DATE} ]" \
+   --cancel-label "Exit" \
+   --menu "Elige una tarea" 14 50 10 \
+   Start "Start App" \
+   Logs "Logs Containers" \
+   Stop "Stop Containers" \
+   Reload "Restart Containers" \
+   Backup "Copia de seguridad de la base de datos" \
+   2>&1 1>&3)
+   exit_status=$?
+   exec 3>&-
+   case ${exit_status} in
+      ${dialogcancel})
+         clear
+         echo "Program terminated." >&2
+         exit
+      ;;
+      ${dialogesc})
+         clear
+         echo "Program aborted." >&2
+         exit 1
+      ;;
+   esac
+   case ${menuitem} in
+      Start)  appStart;;
+      Logs)   appLogs;;
+      Stop)   appStop;;
+      Reload) appStop && appStart;;
+      Backup) copyDatabase;;
+   esac
+done
+##################################################
